7b21cf99e5abc84ca4babdabd7615ed3b03e0ee6
b01t <b01t.mymail@gmail.com>
2019-03-10 19:41:59 +0000

    - Added Second Level Menu Options

---
+++ app/src/main/java/com/passageweather/Constants.java
@@ -0,0 +1,17 @@
+package com.passageweather;
+
+final class Constants {
+
+    /* Navigation Menu */
+    static final int OPTION_ARRAY_SIZE = 9;
+    static final int OPTION_ROOT_INDEX = 0;
+    static final int OPTION_MEDITERRANEAN_INDEX = 1;
+    static final int OPTION_WEST_INDIES_INDEX = 2;
+    static final int OPTION_NORTH_ATLANTIC_INDEX = 3;
+    static final int OPTION_SOUTH_ATLANTIC_INDEX = 4;
+    static final int OPTION_NORTH_PACIFIC_INDEX = 5;
+    static final int OPTION_SOUTH_PACIFIC_INDEX = 6;
+    static final int OPTION_INDIAN_INDEX = 7;
+    static final int OPTION_REGATTA_INDEX = 8;
+
+}
--- app/src/main/java/com/passageweather/OceanAdapter.java
+++ app/src/main/java/com/passageweather/OceanAdapter.java
@@ -2,6 +2,7 @@ package com.passageweather;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +10,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class OceanAdapter extends RecyclerView.Adapter<OceanAdapter.OceanViewHolder> {
-    private int [] mMapIds;
-    private String [] mLabels;
+    private int [][] mMapIds;
+    private String [][] mLabels;
+    private int mLevel;
 
-    public static class OceanViewHolder extends RecyclerView.ViewHolder {
-        public ImageView mImage;
-        public TextView mText;
+    static class OceanViewHolder extends RecyclerView.ViewHolder {
+        ImageView mImage;
+        TextView mText;
 
-        public OceanViewHolder(View view) {
+        OceanViewHolder(View view) {
             super(view);
             mImage = view.findViewById(R.id.iv_option);
             mText = view.findViewById(R.id.tv_option);
@@ -24,7 +26,7 @@ public class OceanAdapter extends RecyclerView.Adapter<OceanAdapter.OceanViewHol
 
     }
 
-    OceanAdapter(int [] mapIds, String [] labels) {
+    OceanAdapter(int [][] mapIds, String [][] labels) {
         mMapIds = mapIds;
         mLabels = labels;
     }
@@ -32,23 +34,34 @@ public class OceanAdapter extends RecyclerView.Adapter<OceanAdapter.OceanViewHol
     @NonNull
     @Override
     public OceanAdapter.OceanViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view =  (View) LayoutInflater.from(viewGroup.getContext())
+        View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.rv_list_item, viewGroup, false);
 
-        OceanViewHolder viewHolder = new OceanViewHolder(view);
-        return viewHolder;
+        return new OceanViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull OceanAdapter.OceanViewHolder holder, int position) {
-        holder.mImage.setImageResource(mMapIds[position]);
-        holder.mText.setText(mLabels[position]);
+        holder.mImage.setImageResource(mMapIds[mLevel][position]);
+        holder.mText.setText(mLabels[mLevel][position]);
+        holder.itemView.setId(position);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLevel(v.getId());
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         if(mLabels == null || mMapIds == null) return 0;
         return mLabels.length;
+    }
+
+    private void setLevel(int level) {
+        mLevel = level;
+        notifyDataSetChanged();
     }
 
 }
