1451ea572dc05de7183a787f3f9060dcf326d246
ajfc84 <ajfc84@protonmail.ch>
2019-04-01 10:22:00 +0100

    Bug fix

--- app/src/main/java/com/passageweather/NavMenuFragment.java
+++ app/src/main/java/com/passageweather/NavMenuFragment.java
@@ -1,6 +1,7 @@
 package com.passageweather;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,35 +37,45 @@ public class NavMenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Activity activity = getActivity();
         if(getArguments() != null) {
             if(getArguments().containsKey(Constants.INTENT_SUBREGION_KEY)){
                 inflater.inflate(R.layout.fragment_great_lake, container);
+                activity.setTitle(R.string.greatlks);
             }
             if(getArguments().containsKey(Constants.INTENT_OPTION_KEY)){
                 switch (getArguments().getInt(Constants.INTENT_OPTION_KEY)) {
                     case Constants.OPTION_MEDITERRANEAN_INDEX:
                         inflater.inflate(R.layout.fragment_mediterranean, container);
+                        activity.setTitle(R.string.med);
                         break;
                     case Constants.OPTION_WEST_INDIES_INDEX:
                         inflater.inflate(R.layout.fragment_west_indies, container);
+                        activity.setTitle(R.string.west);
                         break;
                     case Constants.OPTION_NORTH_ATLANTIC_INDEX:
                         inflater.inflate(R.layout.fragment_north_atlantic, container);
+                        activity.setTitle(R.string.natl);
                         break;
                     case Constants.OPTION_SOUTH_ATLANTIC_INDEX:
                         inflater.inflate(R.layout.fragment_south_atlantic, container);
+                        activity.setTitle(R.string.satl);
                         break;
                     case Constants.OPTION_NORTH_PACIFIC_INDEX:
                         inflater.inflate(R.layout.fragment_north_pacific, container);
+                        activity.setTitle(R.string.npac);
                         break;
                     case Constants.OPTION_SOUTH_PACIFIC_INDEX:
                         inflater.inflate(R.layout.fragment_south_pacific, container);
+                        activity.setTitle(R.string.spac);
                         break;
                     case Constants.OPTION_INDIAN_INDEX:
                         inflater.inflate(R.layout.fragment_indian, container);
+                        activity.setTitle(R.string.ioce);
                         break;
                     case Constants.OPTION_REGATTA_INDEX:
                         inflater.inflate(R.layout.fragment_race_regata, container);
+                        activity.setTitle(R.string.rr);
                         break;
                 }
             }
@@ -78,6 +89,8 @@ public class NavMenuFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        getActivity().setTitle(R.string.app_name);
+        if(getActivity().findViewById(R.id.ll_north_atlantic) != null) getActivity().setTitle(R.string.natl);
+        else getActivity().setTitle(R.string.app_name);
     }
+
 }
